f8a82bd27de89217433ab58830d4211d9d8069aa
Coderica <esm1018@gmail.com>
2015-05-30 15:52:07 -0700

    added the stupid jqm script -_-

--- js/set_marker.js
+++ js/set_marker.js
@@ -4,17 +4,17 @@ $(document).on("pageinit", "#pageMap", function(e, data){
 
   var mapOptions = { zoom: 8 };
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   navigator.geolocation.getCurrentPosition(function(position) {
-      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-      map.setCenter(initialLocation);
+    initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    map.setCenter(initialLocation);
 
-      var marker = new google.maps.Marker({
-        position: initialLocation,
-        map: map,
-      });
+    var marker = new google.maps.Marker({
+      // change current location marker icon
+      position: initialLocation,
+      map: map,
+    });
 
 // Add a space to the database -----------------------------------------
     $('#create-space').on('tap', function(e) {
@@ -31,33 +31,33 @@ $(document).on("pageinit", "#pageMap", function(e, data){
         headers: headers
         }
       ).done(function(response) {
-        console.log(response);
-        alert(response);
+        // drop new marker
+        // notify user of successful or unsuccessful post
+        // console.log(response);
+        // alert(response);
       });
     });
 
-
-
 // Show available spaces from database -----------------------------------------
     // $('#map-canvas').on("tap", function(){
-      var req = $.ajax({
-        url: 'http://mysterious-lake-9849.herokuapp.com',
-        type: "GET",
-      });
+    var req = $.ajax({
+      url: 'http://mysterious-lake-9849.herokuapp.com',
+      type: "GET",
+    });
 
-      req.done(function(response){
-        parkingSpots = response
-        for(i = 0; i < parkingSpots.length; i++){
-          console.log(parkingSpots[i]);
-          var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(parkingSpots[i].latitude,parkingSpots[i].longitude),
-                map: map,
-                title: 'Hello World!',
-                id: parkingSpots[i].id
-          });
-          google.maps.event.addListener(marker, 'click', spaceDetails);
-        };
-      });
+    req.done(function(response){
+      parkingSpots = response
+      for(i = 0; i < parkingSpots.length; i++){
+        console.log(parkingSpots[i]);
+        var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(parkingSpots[i].latitude,parkingSpots[i].longitude),
+              map: map,
+              title: 'Hello World!',
+              id: parkingSpots[i].id
+        });
+        google.maps.event.addListener(marker, 'click', spaceDetails);
+      };
+    });
     // })
     var spaceDetails = function() {
       console.log(this.id)
