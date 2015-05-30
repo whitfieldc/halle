59e61a6a8c8b20f4d45aef8588d424cf6b589919
Charles <whitfield.cw@gmail.com>
2015-05-29 17:47:18 -0700

    ajax callback why doesn't it fire

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@
 /////spanish video version
 
 $(document).on("pageinit", "#pageMap", function(e, data){
-  var map
-  var position = { }
+  var map;
+  // var position = {}
 
   var defaultPos = new google.maps.LatLng(19.289168, -99.653440); //This is currently set to a location in Mexico
 
@@ -47,14 +47,16 @@ $(document).on("pageinit", "#pageMap", function(e, data){
 
   function ShowMap(latlng) {
     var myOptions = {
-      zoom: 16,
+      zoom: 11,
       disableDefaultUI: true, //disables zoom toggle & other defaultUI clutter
       zoomControl: true,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP //sets overlay
     };
 
-    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions); //don't use Jquery here
+    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    //don't use Jquery here
+    // alert(position)
 
     var marker = new google.maps.Marker({
       position: latlng,
@@ -62,29 +64,41 @@ $(document).on("pageinit", "#pageMap", function(e, data){
       title: "My position",
       animation: google.maps.Animation.DROP
     });
+
+
   };
 
-  var setMarker = function(position){
-    // debugger;
+  var setMarker = function(map, position){
     var space = new google.maps.Marker({
       position: new google.maps.LatLng(position.lat, position.lon),
       map: map,
       title: "Space",
       animation: google.maps.Animation.DROP
-    })
+    });
+  };
+
+  var callAjax = function(){
+    var testAjax = $.ajax({
+      url: "http://localhost:3000"
+    });
+
+    var onDone = function(map) {
+      console.log(map)
+
+      return function(response){
+        var position;
+        position.lat = response.spaces[0].latitude
+        position.lon = response.spaces[0].longitude
+        console.log(position);
+        debugger;
+        setMarker(map, position);
+      };
+    };
+    // console.log(map);
+    testAjax.done(onDone(map));
   }
 
-  var testAjax = $.ajax({
-    url: "http://localhost:3000"
-  })
-
-  testAjax.done(function(response){
-    position.lat = response.spaces[0].latitude
-    position.lon = response.spaces[0].longitude
-
-    setMarker(position);
-
-  })
+  $('#map-canvas').on('tap', callAjax);
 
 
 });
