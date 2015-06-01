d01a5156cc25498158a7c4aa89c7556019dc7941
Coderica <esm1018@gmail.com>
2015-06-01 10:59:07 -0700

    facebook oauth & minor refactoring

--- js/set_marker.js
+++ js/set_marker.js
@@ -1,43 +1,48 @@
 $(document).on("pagecreate", "#pageMap", function(e, data){
 
   var ref = new Firebase("https://halle.firebaseio.com");
-  
-  ref.authWithOAuthPopup("facebook", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-    }
+
+// Firebase Facebook login -----------------------------------------
+  $('#login').on('tap', function(e) {
+    e.preventDefault();
+    var ref = new Firebase("https://halle.firebaseio.com");
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
   });
 
+// Format map  -----------------------------------------
   $(".ui-content", this).css({
      height: $(window).height(),
      width: $(window).width()
   });
-
-  // --------------------------------------
 
   var mapOptions = {
         zoom: 13,
         disableDefaultUI: true,
         zoomControl: true
         };
-
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
   navigator.geolocation.getCurrentPosition(function(position) {
+
+
     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
     map.setCenter(initialLocation);
 
-      var spaceId
+      var spaceId //MAKES PARKING SPACE ID AVAILABLE
+
       var marker = new google.maps.Marker({
         position: initialLocation,
         map: map,
         icon: currentLoc
-        //doesn't load on mobile
       });
 
 // Add a space to the database -----------------------------------------
+// TO DO : Add ability to move marker before saving current location as open space -----------------------------------------
     $('#create-space').on('tap', function(e) {
       e.preventDefault();
 
@@ -46,14 +51,14 @@ $(document).on("pagecreate", "#pageMap", function(e, data){
           positionTo: "window",
         })
 
-      // add ability to move marker & add note to parking spot before saving current location as open space
-
       $('#add-space').on('tap', function(e) {
         var longitude = position.coords.longitude;
         var latitude  = position.coords.latitude;
         var note      = $(this).siblings('div').children().val()
         var data      = {space:{longitude:+longitude,latitude:+latitude,note:note}};
         var headers   = '{"Content-Type":"application/json"}';
+        var test = new Firebase('https://halle.firebaseio.com/test');
+
         $.ajax({
           url: 'http://calm-island-3256.herokuapp.com/spaces',
           // url: 'http://localhost:3000/spaces',
@@ -77,6 +82,7 @@ $(document).on("pagecreate", "#pageMap", function(e, data){
                   animation: google.maps.Animation.DROP,
                   zIndex: google.maps.Marker.MAX_ZINDEX + 1
           });
+          test.push({note: response.note});
         }).fail(function(response) {
           console.log(response);
           alert("shits fucked up");
@@ -85,45 +91,36 @@ $(document).on("pagecreate", "#pageMap", function(e, data){
     });
 
 // Show available spaces from database -----------------------------------------
+    $.ajax({
+      url: 'http://calm-island-3256.herokuapp.com',
+      // url: 'http://localhost:3000',
+      type: "GET",
+    }).done(function(response){
+      parkingSpots = response
+      for(i = 0; i < parkingSpots.length; i++){
+        var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(parkingSpots[i].latitude,parkingSpots[i].longitude),
+              map: map,
+              title:  parkingSpots[i].note,
+              icon: markerSelect(parkingSpots[i]), //set marker according to age
+              id: parkingSpots[i].id,
+              creation: parkingSpots[i].converted_time
+        });
+        google.maps.event.addListener(marker, 'click', spaceDetails);
+      };
+    });
 
-      var req = $.ajax({
-        url: 'http://calm-island-3256.herokuapp.com',
-        // url: 'http://localhost:3000',
-        type: "GET",
-      });
-      req.done(function(response){
-        parkingSpots = response
-        for(i = 0; i < parkingSpots.length; i++){
-          console.log(parkingSpots[i].converted_time)
-          console.log(Date.now())
-          console.log(parkingSpots[i]);
-          var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(parkingSpots[i].latitude,parkingSpots[i].longitude),
-                map: map,
-                title:  parkingSpots[i].note, // should this be something else? What if the note is long?
-                // note: parkingSpots[i].note
-                icon: markerSelect(parkingSpots[i]), //set marker according to age
-                id: parkingSpots[i].id,
-                creation: parkingSpots[i].converted_time
-          });
-          google.maps.event.addListener(marker, 'click', spaceDetails);
-        };
-      });
     var spaceDetails = function() {
-      // $('#space-options').text(this.note)
       spaceId = this.id
-      debugger
       $('#space-options').popup("open", {
         overlayTheme: "a",
         positionTo: "window",
       })
       $('p').text(this.title);
-
-      // debugger
     };
 
+// Claim a parking spot  -----------------------------------------
     $('#claim').on('click', function(e){
-      debugger
       e.preventDefault();
       var headers = '{"Content-Type":"application/json"}';
       $.ajax({
@@ -133,7 +130,8 @@ $(document).on("pagecreate", "#pageMap", function(e, data){
         headers: headers,
         data: '' //test without this
       }).done(function(response) {
-        alert("success!")
+        console.log("spot claimed")
+        // add claim confirmation popup?
         // navigation begins
       }).fail(function(response) {
         alert("fuck you guys")
@@ -142,6 +140,7 @@ $(document).on("pagecreate", "#pageMap", function(e, data){
   });
 });
 
+// Space markers CSS -----------------------------------------
 var currentLoc = {
         path: fontawesome.markers.EXCLAMATION,
         scale: 0.65,
@@ -171,6 +170,7 @@ var spaceStale = {
         fillOpacity: 1
     }
 
+// Differentiate between new and stale spaces -----------------------------------------
 var markerSelect = function(spaceObject){
   var creation = spaceObject.converted_time
   if ((Date.now() - creation) <= (5*60000)){
