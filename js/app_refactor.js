a3d15acb0c8bca603d576f755ce8165f6774372b
Charles <whitfield.cw@gmail.com>
2015-06-17 21:33:44 -0700

    change API address to https

--- js/app_refactor.js
+++ js/app_refactor.js
@@ -2,7 +2,7 @@ var ref = new Firebase("https://halle.firebaseio.com");
 var fbData;
 var userData;
 // var baseUrl = 'http://localhost:3000/'
-var baseUrl = 'http://calm-island-3256.herokuapp.com/'
+var baseUrl = 'https://calm-island-3256.herokuapp.com/'
 
 $(document).on("pagecreate", "#landing-screen", function(e, data){
 
@@ -504,7 +504,7 @@ var geocoder = new google.maps.Geocoder();
 
   var address = $('#pac-input').val();
   geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) 
+    if (status == google.maps.GeocoderStatus.OK)
     {
       if (typeof searchMarker !== 'undefined') {
         searchMarker.setMap(null)
@@ -519,8 +519,8 @@ var geocoder = new google.maps.Geocoder();
       });
     markerArray.push(searchMarker)
     map.setZoom(18);
-    } 
-    else 
+    }
+    else
     {
       alert("Location was not found");
     }
